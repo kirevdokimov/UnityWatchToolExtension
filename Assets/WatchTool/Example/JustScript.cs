1c92fe919ccc42d3d2774bb97981c311708043cc
Nobinator <nobinator.root@gmail.com>
2017-08-04 20:42:38 +0300

    Readme added

--- Assets/WatchTool/Example/JustScript.cs
+++ Assets/WatchTool/Example/JustScript.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 using WatchTool;
 
 public class JustScript : MonoBehaviour{
-
-	[Watch] 
+	
+	[Watch] // GameObject and field names
 	public float angle = 7f;
 	
-	[Watch("AwesomeRange")] 
+	[Watch("AwesomeRange")] // GameObject name and custom text
 	public float range = 7f;
 	
-	[Watch("!Global")] 
+	[Watch("!Global")] // Custom text without GameObject name
 	public Vector3 global = Vector3.up;
 
 	[Watch] public string word = "hi";
 	[Watch] public string noword;
 	[Watch] public float nofloat;
 	
-	
 	void Update(){
 		angle += 0.08f;
 		nofloat += 1;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,15 @@
+#Unity WatchTool Extension
+This tool allows you to watch any variable from any MonoBehaviour via [Watch] attribute
+##Usage
+![Usage](usage.gif)
+## Naming rules
+```
+[Watch] // GameObject and field names
+public float angle = 7f;
+	
+[Watch("AwesomeRange")] // GameObject name and custom text
+public float range = 7f;
+	
+[Watch("!Global")] // Custom text without GameObject name
+public Vector3 global = Vector3.up;
+```
